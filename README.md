9e7fa0edf2229a6d0cda849fc00667f0a12842f8
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2022-12-19 22:11:12 -0800

    added information into README, changed certain css aspects, added a class in html and added javascript code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # weather-application
 This is a application that will retrieve the weather in the desired city of you're choosing.
+
+
+My motivation behind this web application is to be able to know the weather in a desired city.
+
+I build this weather application so that when the user is traveling they can look ahead to the city they are wanting to or planning on going to and see how the current weather is.
+
+I learned how to use an api key to pull certain information on the weather and then apply that information to my webpage to show up as the user inputs the city of they're choice.
+
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Global scope variables
 var searchBtn = $('#searchBtn');
 var input = $('#city');
 var townEl = $('#town');
@@ -7,51 +9,57 @@ var airEl = $('#air');
 var speedEl = $('#speed');
 var iconEl = $('#icon');
 
-searchBtn.on('click', function() {
-console.log(input.val());
-getLocation(input.val());
+searchBtn.on('click', function () {
+   console.log(input.val());
+   getLocation(input.val());
+   displayWeather(input.val());
 });
 
 var apiKey = '2d9513d4bfe9acb7cb1f12bbbfd1f381'
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 function getLocation(city) {
-var url = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey
-console.log (url)
-fetch (url)
- .then (function(response) {
-    return response.json()
-    
- }) .then(function (data) {
-    console.log(data[0].lat, data[0].lon)
-    weather(data[0].lat, data[0].lon);
-    
- })
+   var url = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey
+   console.log(url)
+   fetch(url)
+      .then(function (response) {
+         return response.json()
+
+      }).then(function (data) {
+         console.log(data[0].lat, data[0].lon)
+         weather(data[0].lat, data[0].lon);
+
+      })
 }
 
-function weather(lat,lon) {
-    //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    console.log(url)
-  fetch (url) 
-   .then (function(response) {
-    return response.json()
-   }) .then(function(data) {
-       this.displayWeather(data);
-    
-      
-   })
+// this function gets the weather from the pulled lat and lon
+function weather(lat, lon) {
+   //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+   var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+   console.log(url)
+   fetch(url)
+      .then(function (response) {
+         return response.json()
+      }).then(function (data) {
+         this.displayWeather(data);
+
+
+      })
 }
 
+// this function is to display the weather onto the webpage when called
 function displayWeather(data) {
-var { name } = data;
-var { icon, description} = data.weather[0];
-var { temp, humidity } = data.main; 
-var { speed } = data.wind;
-console.log(name,icon,description,temp,humidity,speed);
-town.text("weather in " + name);
-iconEl.text('http://openweathermap.org/img/wn/' + icon + '.png');
-infoEl.text(description);
-degrEl.text (temp + '°F');
-airEl.text("Humidity: " + humidity + '%');
-speedEl.text('Wind speed: ' + speed + 'MPH');
+   var { name } = data;
+      console.log("data.weather" + data.weather);
+      console.log(data + "dataaaaaa");
+   var icon = data.weather[0].icon;
+   var description = data.weather[0].description;
+   var { temp, humidity } = data.main;
+   var { speed } = data.wind;
+     console.log(name, icon, description, temp, humidity, speed);
+       townEl.text("Weather in " + name);
+        iconEl.attr('src', 'http://openweathermap.org/img/wn/' + icon + '.png');
+         infoEl.text(description);
+        degrEl.text(temp + '°F');
+      airEl.text("Humidity: " + humidity + '%');
+    speedEl.text('Wind speed: ' + speed + 'MPH');
 };
